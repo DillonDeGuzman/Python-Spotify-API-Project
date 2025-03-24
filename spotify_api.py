ca416b85855490e0e02452fb2574c03b48b057cf
DillonDeGuzman <DillonDeGuzman@gmail.com>
2025-03-24 08:38:30 -0400

    Added User Interaction
    Added search bar for user to type and search for desired artist or group, it then displays the groups 10 most popular songs in a spotify themed design.

--- spotify_api.py
+++ spotify_api.py
@@ -11,10 +11,10 @@ client_secret = os.getenv("CLIENT_SECRET")
 
 def get_token():
     auth_string = client_id + ":" + client_secret
-    auth_bytes = auth_string.encode("utf-8")  # Fixed: .encode() not .execute()
+    auth_bytes = auth_string.encode("utf-8")
     auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
 
-    url = "https://accounts.spotify.com/api/token"  # Fixed typo "spctify"
+    url = "https://accounts.spotify.com/api/token"
     headers = {
         "Authorization": "Basic " + auth_base64,
         "Content-Type": "application/x-www-form-urlencoded"
@@ -25,12 +25,12 @@ def get_token():
     return json_result["access_token"]
 
 def get_auth_header(token):
-    return {"Authorization": "Bearer " + token}  # Fixed: "Bearer" not "Beaver"
+    return {"Authorization": "Bearer " + token}
 
 def search_for_artist(token, artist_name):
     url = "https://api.spotify.com/v1/search"
     headers = get_auth_header(token)
-    query = f"?q={artist_name}&type=artist&limit=1"  # Fixed query format
+    query = f"?q={artist_name}&type=artist&limit=1"
     
     query_url = url + query
     result = get(query_url, headers=headers)
